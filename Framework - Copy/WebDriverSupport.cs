da6646c9a3a7ea4290c2858dd1ac2ff91e18820b
rishith <rishith7es@gmail.com>
2023-10-14 20:28:22 +1300

    Add project files.

---
+++ Framework - Copy/WebDriverSupport.cs
@@ -0,0 +1,7 @@
+﻿namespace LearningProject
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Framework/WebDriverSupport.cs
@@ -0,0 +1,47 @@
+﻿using BoDi;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace LearningProject
+{
+    [Binding]
+    public class WebDriverSupport : IDisposable
+    {
+        private readonly IObjectContainer objectContainer;
+        private IWait<IWebDriver> _defaultWait = null;
+        private IWebDriver Driver = null;
+
+        public WebDriverSupport(IObjectContainer objectContainer)
+        {
+            this.objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void InitializeWebDriver()
+        {
+            this.Driver = new ChromeDriver();
+            {
+            };
+            objectContainer.RegisterInstanceAs(Driver, typeof(IWebDriver));
+            int timeout = 5000;
+            int pollInterval = 100;
+            _defaultWait = new WebDriverWait(Driver, TimeSpan.FromMilliseconds(timeout))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(pollInterval)
+            };
+            objectContainer.RegisterInstanceAs(_defaultWait, typeof(IWait<IWebDriver>));
+        }
+
+        [AfterScenario]
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                Driver.Quit();
+                Driver.Dispose();
+            }
+        }
+    }
+}
